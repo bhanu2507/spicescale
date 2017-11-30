dad9e4e7909f3aee852e350f183ae0fb6076be75
bhanu2507 <bhanureddy@gmail.com>
2017-11-30 16:45:17 +0530

    upload to firestore

--- webscrape.js
+++ webscrape.js
@@ -4,6 +4,11 @@
 const cheerioReq = require('cheerio-req');
 const Firestore = require('@google-cloud/firestore');
 
+const firestore = new Firestore({
+    projectId: 'spicescale-291ce',
+    keyFilename: './spicescale-0309023d13b5.json',
+  });
+
 cheerioReq('http://www.idlebrain.com//index2.html', (err, $) => {
     //console.log($('#content table tr:nth-child(3) td table tr td table tr td:nth-child(5) table tr:nth-child(5) td p font').html());
     let trailers = [];
@@ -12,53 +17,36 @@ cheerioReq('http://www.idlebrain.com//index2.html', (err, $) => {
     for (let trail of trailers) {
         //console.log(trail.substring(trail.lastIndexOf('="')+1,trail.lastIndexOf('">')).replace('"', ''));
         //console.log('http://www.idlebrain.com/' + trail.substring(trail.lastIndexOf('="')+1,trail.lastIndexOf('">')).replace('"', ''));
-        await cheerioReq('http://www.idlebrain.com/' + trail.substring(trail.lastIndexOf('="')+1,trail.lastIndexOf('">')).replace('"', ''), (err, $) => {
-            console.log($('#fb-root').html());
-        })
+        let trailnm = trail.substring(trail.lastIndexOf('">')+1,trail.lastIndexOf('</a')).replace('>','');
+        //console.log(trailnm, trailnm.lastIndexOf('<'));
+        let trailurl = 'http://www.idlebrain.com/' + trail.substring(trail.lastIndexOf('="')+1,trail.lastIndexOf('">')).replace('"', '');
+        if (trailnm.lastIndexOf('<') == -1) {
+            getytlink(trailurl, function(ytlink){
+                //console.log(trailnm, ytlink);
+                const document = firestore.doc('trailers/'+trailnm);
+                if (ytlink != undefined) {
+                // Enter new data into the document.
+                document.set({
+                    title: trailnm,
+                    url: ytlink,
+                  }).then(() => {
+                    // Document created successfully.
+                  }).catch((error) => {
+                      console.log(error);
+                  });
+                }
+            });
+        }
     }
 
 })
 
 
-/* const firestore = new Firestore({
-    projectId: 'spicescale-291ce',
-    keyFilename: './spicescale-0309023d13b5.json',
-  });
-
-const document = firestore.doc('posts/intro-to-firestore');
-  
-  // Enter new data into the document.
-  document.set({
-    title: 'Welcome to Firestore',
-    body: 'Hello World',
-  }).then(() => {
-    // Document created successfully.
-  }).catch((error) => {
-      console.log(error);
-  }); */
-    
-
-/* const app = express();
-
-app.get('/scrape', function(req, res){
-
-
-
-}) */
-
-/* url = 'http://www.idlebrain.com';
-
-    request(url, function(error, response, html) {
-
-        if(!error){
-            let $ = cheerio.load('<h2 class="title">Hello world</h2>');
-                console.log($("h2.title").text());
-            
-        }
-        else {
-            console.log(error);
-        }
-    }) */
-
-    /* let $ = cheerio.load('<h2 class="title">Hello world</h2>');
-    console.log($("h2.title").text()); */
+async function getytlink (chtml, callback) {
+    let ytlink = '';
+    await cheerioReq(chtml, (err, $) => {
+        //console.log($('body table tr:nth-child(6) td table tr td table tr td:nth-child(2) table tr td p iframe').attr("src"));
+        ytlink = $('body table tr:nth-child(6) td table tr td table tr td:nth-child(2) table tr td p iframe').attr("src");
+    })
+    callback(ytlink);
+}
